759ce84c2481eb0e11b362ac95b19bd5cde6b5c3
majorosbotesz <majoros.botesz@gmail.com>
2023-11-10 14:20:14 +0100

    range

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const button = document.querySelector("button");
 
-
-
-button.onclick = (event) => {
+const inputs = document.querySelectorAll("input");
+const red = document.querySelector("input:first-child");
+const green = document.querySelector("input:nth-child(2)");
+const blue = document.querySelector("input:last-of-type");
+const circle = document.querySelector(".circle");
+inputs.forEach(inp => {
+    inp.addEventListener("input", () =>{
+        circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`
+    })
+})
+/*
+input.onclick = (event) => {
     const red = document.querySelector("input:first-child");
     const green = document.querySelector("input:nth-child(2)");
     const blue = document.querySelector("input:last-of-type");
@@ -10,3 +19,4 @@ button.onclick = (event) => {
 
     circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`
 }
+*/
